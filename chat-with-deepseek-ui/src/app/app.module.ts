4ac2ddd82e5fef86b5744bb8372b7098d31c2554
Emrah <emrahyilm4z@gmail.com>
2025-02-15 03:08:16 +0300

    add header

---
+++ chat-with-deepseek-ui/src/app/app.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {HeaderComponent} from './shared/header/header.component';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule,
+    HeaderComponent
+  ],
+  providers: []
+})
+export class AppModule {
+}
+
